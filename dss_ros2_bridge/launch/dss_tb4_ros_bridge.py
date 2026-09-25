3124cc4f132eb193d2fd341373655dca3cf529d2
kimbyoungkyu <bkkim2004@gmail.com>
2026-09-25 09:27:03 +0900

    touch

--- dss_ros2_bridge/launch/dss_tb4_ros_bridge.py
+++ dss_ros2_bridge/launch/dss_tb4_ros_bridge.py
@@ -4,7 +4,6 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
 
-
 def generate_launch_description():
     common_params = {
         "use_sim_time": True,
